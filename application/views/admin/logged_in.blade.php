2bd8b1bedad4eb8a48ac2fc277df04e18e3173f6
Dmitriy A. Golev <ceo@veliov.com>
2013-03-25 03:48:14 +0400

    Styles upd

--- application/views/admin/logged_in.blade.php
+++ application/views/admin/logged_in.blade.php
@@ -1,6 +1,10 @@
 @layout('admin.template')
 
 @section('content_area')
-	<h1><i class="icon-th"></i> Welcome to Indira<sup><i class="icon-lemon"></i></sup><sub>CMS</sub></h1>
+
+	<center>
+		<h1><i class="icon-th"></i> Welcome to Indira<sup><i class="icon-lemon"></i></sup><sub>CMS</sub></h1>
+	</center>
+
 @parent
 @endsection
