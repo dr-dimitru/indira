cdd160aeef467c1ac16d7f3859fd7424798a2984
Dmitriy A. Golev <ceo@veliov.com>
2013-04-13 06:31:56 +0400

    Build v. 0.0.21

--- application/language/en/promo.php
+++ application/language/en/promo.php
@@ -9,7 +9,7 @@
 			Or check souce code at GitHub: <br />
 			<a href="https://github.com/dr-dimitru/indira" onclick=" _gaq.push([\'_trackEvent\', \'github\', \'go_to_github\', \'View on GitHub\']);" class="btn btn-small"><i class="icon-github-alt icon-white"></i> Indira on GitHub</a>',
 		'view_on_git' => 'View project on GitHub',
-		'download' => 'Download Indira <sup><i class="icon-lemon"></i></sup> <small>(v. 0.0.12)</small>',
+		'download' => 'Download Indira <sup><i class="icon-lemon"></i></sup> <small>(v. 0.0.21)</small>',
 		'try_it' => 'Try it now!',
 		'laravel' => 'Indira<sup><i class="icon-lemon"></i></sup> CMS is based on <a href="http://laravel.com/" target="_blank">Laravel</a>. Your developer team will love it! Cause: 
 			<blockquote>Laravel is a clean and classy framework for PHP web development. Freeing you from spaghetti code, Laravel helps you create wonderful applications using simple, expressive syntax. Development should be a creative experience that you enjoy, not something that is painful. Enjoy the fresh air. <small>Main page <cite title="A Framework For Web Artisans">Laravel.com</cite></small></blockquote><a href="http://laravel.com" onclick=" _gaq.push([\'_trackEvent\', \'laravel\', \'go_to_laravel.com\', \'\']);" target="_blank" class="btn btn-small">Find out more <i class="icon-external-link"></i></a>',
