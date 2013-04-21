4f27ae03f74f6089c885e6b3ee96cd6aeaf25d3d
Dmitriy A. Golev <ceo@veliov.com>
2013-04-21 22:48:22 +0400

    Installation Guide
    Now comes with every Indira CMS by default

---
+++ storage/db/sections/1.php
@@ -0,0 +1,5 @@
+<?php 
+return array( 'id' => 1, 
+'title' => 'Installation%20%26%20Setting%20up', 
+'lang' => 'en', 
+ );
---
+++ storage/db/sections/2.php
@@ -0,0 +1,5 @@
+<?php 
+return array( 'id' => 2, 
+'title' => '%D0%9D%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0%20%D0%B8%20%D1%83%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0', 
+'lang' => 'ru', 
+ );
