92ff5f949153882e6beb1d617c1ec4dad46dd5fe
Dmitriy A. Golev <ceo@veliov.com>
2013-08-22 22:06:29 +0400

    UPD Indira Version to 1.0.13

--- storage/db/settings/1.php
+++ storage/db/settings/1.php
@@ -1,2 +1,2 @@
 <?php 
-return array( 'id' => 1, 'created_at' => '2013-05-15%2012%3A18%3A10', 'updated_at' => '1377014508', 'type' => 'indira', 'param' => 'indira_version', 'value' => '1.0.12',  );
+return array( 'id' => 1, 'created_at' => '2013-05-15%2012%3A18%3A10', 'updated_at' => '1375096357', 'type' => 'indira', 'param' => 'indira_version', 'value' => '1.0.13',  );
