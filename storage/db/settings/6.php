7012ad5c23695f0926e8c9e47037a24162f8ae3e
Dmitriy A. Golev <ceo@veliov.com>
2013-08-26 23:59:54 +0400

    Remove unused settings

--- storage/db/settings/6.php
+++
@@ -1,2 +0,0 @@
-<?php 
-return array( 'id' => 6, 'created_at' => '2013-05-15%2017%3A05%3A14', 'updated_at' => '1371087951', 'type' => 'indira', 'param' => 'google_analytics', 'value' => 'UA-12784007-24',  );
