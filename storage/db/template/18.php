2f33f0f7b73bcb1cf70ba1872db519cc20262fb8
Dmitriy A. Golev <ceo@veliov.com>
2013-08-28 21:08:23 +0400

    Remove setting

--- storage/db/template/18.php
+++ storage/db/template/18.php
@@ -1,2 +1,2 @@
 <?php 
-return array( 'id' => 18, 'created_at' => '1372269243', 'updated_at' => '1372269287', 'type' => 'google_analytics', 'name' => 'tracking_code', 'value' => 'UA-12784007-24', 'additions' => '', 'order' => '7',  );
+return array( 'id' => 18, 'created_at' => '1372269243', 'updated_at' => '1372269287', 'type' => 'google_analytics', 'name' => 'tracking_code', 'value' => '', 'additions' => '', 'order' => '7',  );
