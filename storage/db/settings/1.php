03e2ee6a99c660fb211274cda9e2daa05d99a49c
Dmitriy A. Golev <ceo@veliov.com>
2013-08-28 09:35:06 +0400

    UPD inidra version to 1.0.24

--- storage/db/settings/1.php
+++ storage/db/settings/1.php
@@ -1,2 +1,2 @@
 <?php 
-return array( 'id' => 1, 'created_at' => '2013-05-15%2012%3A18%3A10', 'updated_at' => '1375096357', 'type' => 'indira', 'param' => 'indira_version', 'value' => '1.0.23',  );
+return array( 'id' => 1, 'created_at' => '2013-05-15%2012%3A18%3A10', 'updated_at' => '1375096357', 'type' => 'indira', 'param' => 'indira_version', 'value' => '1.0.24',  );
