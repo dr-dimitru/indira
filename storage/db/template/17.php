0197988419f06b17b8e0763656445f5817e98586
Dmitriy A. Golev <ceo@veliov.com>
2013-08-28 21:09:43 +0400

    Remove default values in twitter settings

--- storage/db/template/17.php
+++ storage/db/template/17.php
@@ -1,2 +1,2 @@
 <?php 
-return array( 'id' => 17, 'created_at' => '1372031694', 'updated_at' => '1372031787', 'type' => 'meta', 'name' => 'twitter%3Acreator', 'value' => 'smart_egg', 'additions' => '', 'order' => '4',  );
+return array( 'id' => 17, 'created_at' => '1372031694', 'updated_at' => '1372031787', 'type' => 'meta', 'name' => 'twitter%3Acreator', 'value' => '', 'additions' => '', 'order' => '4',  );
